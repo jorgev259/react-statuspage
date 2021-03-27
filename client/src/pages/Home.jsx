ccb852ec8a1f79b2d4ab3066c82bcf3b802f0d84
jorgev259 <jorgev259@outlook.es>
2021-03-27 16:58:19 -0600

    [Client] add loading indicators

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { gql, useQuery } from '@apollo/client'
 import { Col, Row } from 'react-bootstrap'
 import { SiteRow } from './helpers'
 import classnames from 'classnames'
+import Loader from 'react-loaders'
 
 const servicesQuery = gql`
   query Services {
@@ -30,7 +31,7 @@ const overallQuery = gql`
 `
 
 export default function Home () {
-  const { data } = useQuery(servicesQuery)
+  const { data, loading } = useQuery(servicesQuery)
 
   const allGood = data && data.services.every(r => r.state)
   const allBad = data && data.services.every(r => !r.state)
@@ -38,87 +39,100 @@ export default function Home () {
   return (
     <>
       <Row className={classnames('mainBox', { yellow: !allBad && !allGood, red: allBad, lightgreen: allGood })}>
-        <Col className='d-flex align-items-center'>
-          <div className='status mr-2' style={{ width: '20px', height: '20px' }} />
-          <h2 className='my-auto' style={{ fontWeight: 'bold' }}>
-            {!allBad && !allGood
-              ? 'Some systems down'
-              : (
-                  allGood ? 'All systems operational' : 'All systems down'
-                )}
-          </h2>
-        </Col>
+        {loading
+          ? <Col className='d-flex justify-content-center'><Loader type='pacman' active /></Col>
+          : (
+            <Col className={classnames('d-flex align-items-center', { 'justify-content-center': loading })}>
+              <div className='status mr-2' style={{ width: '20px', height: '20px' }} />
+              <h2 className='my-auto' style={{ fontWeight: 'bold' }}>
+                {!allBad && !allGood
+                  ? 'Some systems down'
+                  : (
+                      allGood ? 'All systems operational' : 'All systems down'
+                    )}
+              </h2>
+            </Col>
+            )}
       </Row>
 
       <Row className='mt-5'><h4>Services</h4></Row>
       <Row className='mainBox'>
-        <Col>
-          {data && data.services.map((service, i) => (
-            <SiteRow key={service.id} service={service} first={i === 0} last={i === data.services.length - 1} nameFlag />
-          ))}
-        </Col>
+        {loading
+          ? <Col className='d-flex justify-content-center'><Loader type='pacman' active /></Col>
+          : (
+            <Col>
+              {data.services.map((service, i) => (
+                <SiteRow key={service.id} service={service} first={i === 0} last={i === data.services.length - 1} nameFlag />
+              ))}
+            </Col>
+            )}
       </Row>
-
       <Overall />
     </>
   )
 }
 
 function Overall () {
-  const { data } = useQuery(overallQuery)
+  const { data, loading } = useQuery(overallQuery)
 
   return (
     <>
       <Row><h4 className='mt-5'>Overall Uptime</h4></Row>
       <Row className='mainBox overall'>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.uptime1.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 24 hours
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.uptime7.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 7 days
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.uptime30.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 30 days
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.uptime90.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 90 days
-            </Col>
-          </Row>
-        </Col>
+        {loading
+          ? <Col className='d-flex justify-content-center'><Loader type='pacman' active /></Col>
+          : (
+            <>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.uptime1.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 24 hours
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.uptime7.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 7 days
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.uptime30.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 30 days
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.uptime90.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 90 days
+                  </Col>
+                </Row>
+              </Col>
+            </>
+            )}
       </Row>
     </>
   )
--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from '@apollo/client'
 import { Col, Row } from 'react-bootstrap'
 import { SiteRow } from './helpers'
+import Loader from 'react-loaders'
 
 const serviceQuery = gql`
   query Services($id: ID!) {
@@ -31,127 +32,143 @@ const serviceQuery = gql`
 `
 
 export default function Home (props) {
-  const { data } = useQuery(serviceQuery, { variables: { id: props.id } })
+  const { data, loading } = useQuery(serviceQuery, { variables: { id: props.id } })
 
   return (
     <>
       <h4>Uptime Last 90 days</h4>
       <Row className='mainBox'>
-        <Col>
-          {data && <SiteRow key={data.service.id} service={data.service} first last />}
-        </Col>
+        {loading
+          ? <Col className='d-flex justify-content-center'><Loader type='pacman' active /></Col>
+          : (
+            <Col>
+              {data && <SiteRow key={data.service.id} service={data.service} first last />}
+            </Col>
+            )}
       </Row>
 
-      <Overall data={data} />
-      <ResponseTime data={data} />
+      <Overall data={data} loading={loading} />
+      <ResponseTime data={data} loading={loading} />
     </>
   )
 }
 
 function Overall (props) {
-  const { data } = props
+  const { data, loading } = props
 
   return (
     <>
       <h4 className='mt-5'>Overall Uptime</h4>
       <Row className='mainBox overall'>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.service.uptime1.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 24 hours
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.service.uptime7.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 7 days
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.service.uptime30.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 30 days
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row className='number'>
-            <Col>
-              {data && data.service.uptime90.toFixed(3)}%
-            </Col>
-          </Row>
-          <Row className='title'>
-            <Col>
-              Last 90 days
-            </Col>
-          </Row>
-        </Col>
+        {loading
+          ? <Col className='d-flex justify-content-center'><Loader type='pacman' active /></Col>
+          : (
+            <>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.service.uptime1.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 24 hours
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.service.uptime7.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 7 days
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.service.uptime30.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 30 days
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className='number'>
+                  <Col>
+                    {data && data.service.uptime90.toFixed(3)}%
+                  </Col>
+                </Row>
+                <Row className='title'>
+                  <Col>
+                    Last 90 days
+                  </Col>
+                </Row>
+              </Col>
+            </>
+            )}
       </Row>
     </>
   )
 }
 
 function ResponseTime (props) {
-  const { data } = props
+  const { data, loading } = props
   return (
     <>
       <h4 className='mt-5'>Response Time Last 2 days </h4>
       <Row className='mainBox overall'>
         <Col style={{ border: 'none' }}>
           <Row>
-            <Col>
-              <Row className='number'>
-                <Col>
-                  {data && data.service.response.avg.toFixed(2)}ms
-                </Col>
-              </Row>
-              <Row className='title'>
-                <Col>
-                  Avg. response time
-                </Col>
-              </Row>
-            </Col>
-            <Col>
-              <Row className='number'>
-                <Col>
-                  {data && data.service.response.max.toFixed(2)}ms
-                </Col>
-              </Row>
-              <Row className='title'>
-                <Col>
-                  Max. response time
-                </Col>
-              </Row>
-            </Col>
-            <Col>
-              <Row className='number'>
-                <Col>
-                  {data && data.service.response.min.toFixed(2)}ms
-                </Col>
-              </Row>
-              <Row className='title'>
-                <Col>
-                  Min. response time
-                </Col>
-              </Row>
-            </Col>
+            {loading
+              ? <Col className='d-flex justify-content-center'><Loader type='pacman' active /></Col>
+              : (
+                <>
+                  <Col>
+                    <Row className='number'>
+                      <Col>
+                        {data && data.service.response.avg.toFixed(2)}ms
+                      </Col>
+                    </Row>
+                    <Row className='title'>
+                      <Col>
+                        Avg. response time
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col>
+                    <Row className='number'>
+                      <Col>
+                        {data && data.service.response.max.toFixed(2)}ms
+                      </Col>
+                    </Row>
+                    <Row className='title'>
+                      <Col>
+                        Max. response time
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col>
+                    <Row className='number'>
+                      <Col>
+                        {data && data.service.response.min.toFixed(2)}ms
+                      </Col>
+                    </Row>
+                    <Row className='title'>
+                      <Col>
+                        Min. response time
+                      </Col>
+                    </Row>
+                  </Col>
+                </>
+                )}
           </Row>
         </Col>
       </Row>
