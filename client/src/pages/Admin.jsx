9228e897fde22dfce248a5f3108a0e4d9e346780
jorgev259 <jorgev259@outlook.es>
2021-09-12 18:30:45 -0600

    [Client/Server] more admin forms

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,10 +1,11 @@
 
-import { useMutation, gql } from '@apollo/client'
+import { useMutation, gql, useQuery } from '@apollo/client'
 import { toast } from 'react-toastify'
 import { Col, Form, Button, Row, FormGroup } from 'react-bootstrap'
 import serialize from 'form-serialize'
 import Cookies from 'js-cookie'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 const servicesMutation = gql`
   mutation createService($name: String!, $url: String!, $interval: Float!, $timeout: Float!, $ping: Boolean!) {
@@ -14,9 +15,54 @@ const servicesMutation = gql`
   }
 `
 
+const updateMutation = gql`
+  mutation updateService($id: ID!, $options: ServiceUpdateOptions!) {
+    updateService(id:$id, options: $options){
+      id
+      name
+    }
+  }
+`
+
+const orderMutation = gql`
+  mutation updateOrder($id: ID!, $destination: Int!) {
+    updateOrder(id:$id, destination: $destination)
+  }
+`
+
+const servicesQuery = gql`
+  query Services {
+    services {
+      id
+      name
+      url
+      interval
+      timeout
+      order
+      ping
+      state
+    }
+  }
+`
+
 export default function Admin () {
   const [logged, setLogged] = useState(false)
-  return logged ? <AdminForm /> : <LoginForm setLogged={setLogged} />
+  const { data, refetch } = useQuery(servicesQuery, { fetchPolicy: 'network-only' })
+  const props = { data, refetch }
+
+  if (!logged) return <LoginForm setLogged={setLogged} />
+
+  return (
+    <>
+      <AdminForm />
+      {data && (
+        <>
+          <EditService {...props} />
+          <EditOrder {...props} />
+        </>
+      )}
+    </>
+  )
 }
 
 function LoginForm ({ setLogged }) {
@@ -25,6 +71,7 @@ function LoginForm ({ setLogged }) {
     login(key: $key)
   }
 `
+
   const [login, { loading }] = useMutation(query)
   const loader = ''
 
@@ -130,5 +177,160 @@ function AdminForm () {
         </Col>
       </Form.Row>
     </Form>
+  )
+}
+
+function EditService ({ data, refetch }) {
+  const [mutate] = useMutation(updateMutation)
+  const services = [...data.services].sort((a, b) => a.id - b.id)
+  const [item, setItem] = useState(services[0])
+  const formRef = useRef(null)
+
+  useEffect(() => formRef.current.reset(), [item])
+
+  function handleSubmitForm (e) {
+    e.preventDefault()
+    e.persist()
+
+    const variables = serialize(e.target, { hash: true })
+    variables.interval = parseFloat(variables.interval)
+    variables.timeout = parseFloat(variables.timeout)
+    variables.ping = variables.ping === 'on'
+
+    for (const key of Object.keys(variables)) {
+      if (item[key] === variables[key]) delete variables[key]
+    }
+
+    if (Object.keys(variables).length === 0) return
+
+    mutate({ variables: { id: item.id, options: variables } }).then(result => {
+      toast.success(`Updated service "${result.data.updateService.name}" succesfully!`)
+      e.target.reset()
+      setTimeout(() => refetch(), 10 * 1000)
+    }).catch(err => {
+      console.log(err)
+      toast.error(err.message, { autoclose: false })
+    })
+  }
+
+  return (
+    <div className='mainBox p-4 mt-3'>
+      <Form.Row className='w-100'>
+        <Col xs='12'>
+          <Form.Group>
+            <Form.Control as='select' onChange={ev => setItem(services.find(s => s.id === ev.target.value))}>
+              {services.map(s => <option key={s.id} value={s.id}>{`(${s.id}) ${s.name}`}</option>)}
+            </Form.Control>
+          </Form.Group>
+        </Col>
+      </Form.Row>
+
+      <Form inline className='mt-2' onSubmit={handleSubmitForm} ref={formRef}>
+        <Form.Row className='w-100 pb-3'>
+          <Col xs='12' md='4'>
+            <Form.Group>
+              <Form.Label className='mr-2'>Name:</Form.Label>
+              <Form.Control type='text' name='name' required defaultValue={item.name} />
+            </Form.Group>
+          </Col>
+          <Col xs='12' md='4'>
+            <Form.Group>
+              <Form.Label className='mr-2'>Url:</Form.Label>
+              <Form.Control type='text' name='url' required defaultValue={item.url} />
+            </Form.Group>
+          </Col>
+          <Col xs='12' md='4'>
+            <Form.Group>
+              <Form.Label className='mr-2'>Ping mode:</Form.Label>
+              <Form.Control type='checkbox' name='ping' defaultChecked={item.ping} />
+            </Form.Group>
+          </Col>
+        </Form.Row>
+
+        <Form.Row className='w-100'>
+          <Col xs='12' md='4'>
+            <Form.Group>
+              <Form.Label className='mr-2'>Interval:</Form.Label>
+              <Form.Control type='number' min={0} name='interval' required defaultValue={item.interval} />
+            </Form.Group>
+          </Col>
+          <Col xs='12' md='4'>
+            <Form.Group>
+              <Form.Label className='mr-2'>Timeout:</Form.Label>
+              <Form.Control type='number' min={0} name='timeout' required defaultValue={item.timeout} />
+            </Form.Group>
+          </Col>
+          <Col xs='auto'>
+            <Button type='submit'>
+              Add Service
+            </Button>
+          </Col>
+        </Form.Row>
+      </Form>
+    </div>
+  )
+}
+
+function EditOrder ({ data, refetch }) {
+  const [update] = useMutation(orderMutation)
+  const services = [...data.services].sort((a, b) => a.order - b.order)
+
+  const grid = 8
+
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+    ...draggableStyle
+  })
+
+  const getListStyle = isDraggingOver => ({ padding: grid })
+
+  function onDragEnd (result) {
+    if (!result.destination) return
+    update({ variables: { id: result.draggableId, destination: result.destination.index } })
+      .then(() => toast.success('Update succesful!'))
+      .catch(error => {
+        console.log(error)
+        toast.error('Update failed')
+      })
+      .finally(refetch)
+  }
+
+  return (
+    <div className='row mainBox py-0 px-0 mt-3'>
+      <div className='col px-0'>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId='droppable'>
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps || {}}
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {services.map(s => (
+                  <Draggable key={s.id} draggableId={s.id} index={s.order}>
+                    {(provided, snapshot) => (
+                      <div
+                        className='w-100 py-3 px-4 orderRow' key={s.id}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps ? provided.draggableProps.style : {}
+                        )}
+                      >
+                        <h5 className='d-inline align-middle'>{s.name}</h5>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+    </div>
   )
 }
--- server/graphql/resolvers/Mutation.js
+++ server/graphql/resolvers/Mutation.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken')
 const { AuthenticationError } = require('apollo-server-express')
 const Urlmon = require('url-monitor')
 const submit = require('../../submitFn')
-
-console.log(submit)
+const { Op } = require('sequelize')
 
 module.exports = {
   createService: async (parent, args, { db, user }, info) => {
@@ -18,6 +17,37 @@ module.exports = {
 
     return service
   },
+  updateService: async (parent, { id, options }, { db, user }) => {
+    if (!user) throw new AuthenticationError()
+
+    const row = await db.models.service.findByPk(id)
+    for (const [key, value] of Object.entries(options)) {
+      row[key] = value
+    }
+    await row.save()
+    setTimeout(() => process.exit(), 10 * 1000)
+    return row
+  },
+  updateOrder: async (parent, { id, destination }, { db, user }) => {
+    if (!user) throw new AuthenticationError()
+
+    db.transaction(async (t1) => {
+      const item = await db.models.service.findByPk(id)
+      const verb = destination < item.order ? 'increment' : 'decrement'
+      const options = destination < item.order
+        ? { [Op.and]: [{ order: { [Op.lt]: item.order } }, { order: { [Op.gte]: destination } }] }
+        : { [Op.and]: [{ order: { [Op.gt]: item.order } }, { order: { [Op.lte]: destination } }] }
+
+      const rows = await db.models.service.findAll({ where: options })
+      await Promise.all(rows.map(r => r[verb]('order', { by: 1 })))
+
+      item.order = destination
+      await item.save()
+
+      return 1
+    })
+  },
+
   login: (parent, { key }, context) => {
     if (process.env.ADMIN_KEY === key) return jwt.sign({}, process.env.JWT_SECRET, { algorithm: 'HS256', expiresIn: '1d' })
     else throw new Error('Failed login')
@@ -40,5 +70,5 @@ function startService (service, db) {
     submit(db, id, data, false)
   })
 
-  website.start()
+  // website.start()
 }
